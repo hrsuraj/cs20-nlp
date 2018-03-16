2a44d813d617bb8d26f2ed0e959cfd9a4c30922d
Suraj Heereguppe <hrsuraj@stanford.edu>
2018-03-16 00:04:25 -0700

    waste

--- visualize.py
+++ visualize.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
     # Load model of last epoch
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        saver.restore(sess, '../word_vec_models/epoch_30/model.ckpt')
+        saver.restore(sess, '../word_vec_models/epoch_1/model.ckpt')
         # Visualizing embeddings
-        final_embed = sess.run(model.init_vecs)
+        # final_embed = dill.load(open('word_vecs','rb'))
         config = projector.ProjectorConfig()
-        summary_writer = tf.summary.FileWriter('final embeddings')
+        summary_writer = tf.summary.FileWriter('final_embeddings')
         embedding = config.embeddings.add()
         embedding.tensor_name = model.init_vecs.name
         embedding.metadata_path = './embeddings_metadata.tsv'
         projector.visualize_embeddings(summary_writer, config)
         saver_embed = tf.train.Saver([model.init_vecs])
         saver_embed.save(sess, '../word_vec_graphs/projector.ckpt')
-        dill.dump(final_embed, open('word_vecs','wb'))
+        # dill.dump(final_embed, open('word_vecs','wb'))
