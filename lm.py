7384c68599388334d19de64f99a6f98cabbe967a
Chiraag Sumanth <chiraagsumanth@DN0a226911.SUNet>
2018-03-15 17:38:32 -0700

    LM predict

--- lm.py
+++ lm.py
@@ -53,7 +53,7 @@ class LanguageModel(object):
         self.in_state = tuple([tf.placeholder_with_default(state, [None, state.shape[1]]) 
                                 for state in zero_states])
     
-        self.output, _ = tf.nn.dynamic_rnn(cells, self.inputs, self.length_max(self.inputs), self.next_state)
+        self.output, _ = tf.nn.dynamic_rnn(cells, self.inputs, self.length_max(self.inputs), self.in_state)
 
         self.logits = tf.layers.dense(self.output, self.vocab_len, activation=None)
 
