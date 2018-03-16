c3c72b296855ab8776b552ae823239bda85ae1b9
Suraj Heereguppe <hrsuraj@stanford.edu>
2018-03-16 00:32:57 -0700

    make more generic

--- embeddings_metadata.py
+++ embeddings_metadata.py
@@ -3,12 +3,16 @@ import dill
 
 ###############################################################################
 
-def create_embeddings_metadata(path2dict, save_path):
+def create_embeddings_metadata(path2dict, save_path, num_words):
     idx2word = dill.load(open(path2dict, 'rb'))
     with open(save_path, 'w') as f:
         f.write('Index\tWord\n')
-        for idx in range(len(idx2word)):
-            f.write(str(idx) + '\t' + idx2word[idx] + '\n')
+        if num_words == -1:
+            for idx in range(len(idx2word)):
+                f.write(str(idx) + '\t' + idx2word[idx] + '\n')
+        else:
+            for idx in range(num_words):
+                f.write(str(idx) + '\t' + idx2word[idx] + '\n')
 
 ###############################################################################
 
@@ -16,6 +20,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read embeddings dict and create metadata for the embeddings visualization')
     parser.add_argument('--idx2word', required=True, dest='idx2word')
     parser.add_argument('--savepath', required=True, dest='save_path')
+    parser.add_argument('--num_words', required=False, type=int, default=-1, dest='num_words')
     args = parser.parse_args()
     
-    create_embeddings_metadata(path2dict=args.idx2word, save_path=args.save_path)
+    create_embeddings_metadata(path2dict=args.idx2word, save_path=args.save_path, num_words=args.num_words)
--- visualize.py
+++ visualize.py
@@ -1,4 +1,6 @@
+import argparse
 import dill
+import os
 import tensorflow as tf
 
 from cbow import CBOW
@@ -8,20 +10,31 @@ from tensorflow.contrib.tensorboard.plugins import projector
 
 if __name__ == '__main__':
     
+    parser = argparse.ArgumentParser(description='Visualize word vectors with tensorboard')
+    parser.add_argument('--init_vecs', required=True, dest='init_vecs')
+    parser.add_argument('--model_ckpt', required=True, dest='model_ckpt')
+    parser.add_argument('--num_embed', required=True, type=int, dest='num_embed')
+    parser.add_argument('--embed_metadata', required=True, dest='metadata')
+    parser.add_argument('--graph_folder', required=True, dest='graph_folder')
+    args = parser.parse_args()
+    
     # Initialize model
-    word_vectors = dill.load(open('word_vectors','rb'))
+    word_vectors = dill.load(open(args.init_vecs,'rb'))
     model = CBOW(len(word_vectors), word_vectors, 5e-4)
     
     # Load model of last epoch
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        saver.restore(sess, '../word_vec_models/epoch_1/model.ckpt')
+        saver.restore(sess, args.model_ckpt)
         # Visualizing embeddings
+        final_embed = sess.run(model.init_vecs)
+        embedding_var = tf.Variable(final_embed_matrix[:args.num_embed], name='embedding')
+        sess.run(embedding_var.initializer)
         config = projector.ProjectorConfig()
         summary_writer = tf.summary.FileWriter('final_embeddings')
         embedding = config.embeddings.add()
-        embedding.tensor_name = model.init_vecs.name
-        embedding.metadata_path = './embeddings_metadata.tsv'
+        embedding.tensor_name = embedding_var.name
+        embedding.metadata_path = args.metadata
         projector.visualize_embeddings(summary_writer, config)
-        saver_embed = tf.train.Saver([model.init_vecs])
-        saver_embed.save(sess, '../word_vec_graphs/projector.ckpt')
+        saver_embed = tf.train.Saver([embedding_var])
+        saver_embed.save(sess, os.path.join(args.graph_folder, 'projector.ckpt'))
