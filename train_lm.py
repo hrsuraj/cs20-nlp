664ddbf61b8b04ea5f0f7edbf691e5c19a7c229a
Chiraag Sumanth <chiraagsumanth@DN0a226911.SUNet>
2018-03-15 18:27:05 -0700

    LM predict

--- train_lm.py
+++ train_lm.py
@@ -55,8 +55,7 @@ if __name__ == '__main__':
                 feed_dict = model.create_feed_dict(inputs=inputs, in_state = init_state)   
                 probs, in_state, next_state = sess.run([model.logits, model.in_state, model.next_state], feed_dict = feed_dict)
                 init_state = next_state
-                print np.argsort(-probs)[:5]
-                op_words.append(i2w[np.argmax(probs.shape)])
+                op_words.append(i2w[np.argmax(probs)])
                 inputs = np.array([[word_vector[w2i[op_words[-1]]]]])
 
                 # print init_state
