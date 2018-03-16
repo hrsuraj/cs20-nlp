65fd6c871e0b7c10530872b81fa567694b9e1dce
Suraj Heereguppe <hrsuraj@stanford.edu>
2018-03-15 22:35:26 -0700

    git is pain

---
+++ visualize.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+
+from cbow import CBOW
+
+###############################################################################
+
+if __name__ == '__main__':
+    
+    # Initialize model
+    word_vectors = dill.load(open('word_vectors','rb'))
+    model = CBOW(len(word_vectors), word_vectors, 5e-4)
+    
+    # Load model of last epoch
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, '../word_vec_models/epoch_100/model.ckpt')
+        # Visualizing embeddings
+        final_embed = sess.run(model.word_vecs)
+        config = projector.ProjectorConfig()
+        summary_writer = tf.summary.FileWriter('final embeddings')
+        embedding = config.embeddings.add()
+        embedding.tensor_name = model.word_vecs.name
+        embedding.metadata_path = embed_data_path
+        projector.visualize_embeddings(summary_writer, config)
+        saver_embed = tf.train.Saver([model.word_vecs])
+        saver_embed.save(sess, '../word_vec_graphs/')
